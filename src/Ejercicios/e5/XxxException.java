4c7e3738addd0b6939a2640acfd8e001eff83057
laura <l.rubioh@edu.uah.es>
2025-04-02 18:44:05 +0200

    Ejercicio 5 y Test

---
+++ src/Ejercicios/e5/XxxException.java
@@ -0,0 +1,7 @@
+package Ejercicios.e5;
+
+public class XxxException extends Exception {
+    public XxxException(String message) {
+        super(message);
+    }
+}
---
+++ src/Ejercicios/e5/YyyException.java
@@ -0,0 +1,8 @@
+package Ejercicios.e5;
+
+public class YyyException extends Exception {
+    public YyyException(String message) {
+        super(message);
+
+    }
+}
